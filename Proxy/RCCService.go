69547278ec2cb0679b04b571e07935ab0417bffa
Lewin Kelly <heli@odex.cf>
2025-10-06 06:54:01 +0100

    Improved error handling and formatting

--- Proxy/RCCService.go
+++ Proxy/RCCService.go
@@ -16,6 +16,7 @@ import (
 	"embed"
 	"encoding/base64"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"io"
 	"net"
@@ -34,7 +35,6 @@ import (
 
 var (
 	_      embed.FS // force embed import hack
-	wg     sync.WaitGroup
 	client http.Client
 	//go:embed soap.xml
 	template string
@@ -51,23 +51,23 @@ func Logr(txt string) {
 
 var cmd *exec.Cmd
 
-func Assert(err error, txt string) {
+func Fatal(err error, txt string) {
 	// so that I don't have to write this every time
-	// todo: should only be used for fatal errors
-	if err != nil {
-		fmt.Println(err)
-		Log(c.InRed(txt))
-		if cmd != nil && cmd.Process != nil && cmd.Process.Pid != 0 {
-			Log(c.InRed("Killing RCCService..."))
-			cmd.Process.Kill()
-		}
-		os.Exit(1)
+	if err == nil {
+		return
 	}
+	fmt.Println(err)
+	Log(c.InRed(txt))
+	if cmd != nil && cmd.Process != nil && cmd.Process.Pid != 0 {
+		Log(c.InRed("Killing RCCService..."))
+		cmd.Process.Kill()
+	}
+	os.Exit(1)
 }
 
 func StartRCC() {
 	_, err := os.Stat("./RCCService/RCCService.exe")
-	Assert(err, "RCCService.exe not found! Please place the RCCService folder in the current directory.")
+	Fatal(err, "RCCService.exe not found! Please place the RCCService folder in the current directory.")
 	for {
 		if runtime.GOOS == "windows" {
 			cmd = exec.Command("./RCCService/RCCService.exe", "-Console")
@@ -80,35 +80,39 @@ func StartRCC() {
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		cmd.Run()
+		fmt.Println(cmd.Err)
 		Log(c.InRed("RCCService has stopped. Restarting..."))
 	}
 }
 
 func TestRCCStarted(loaded *bool, t int) {
 	Logr(c.InPurple(fmt.Sprintf("Waiting for RCCService to start... (%ds)", t)))
-	_, err := http.Get("http://localhost:64989")
-	if err == nil {
+	if _, err := http.Get("http://localhost:64989"); err == nil {
 		*loaded = true
 	}
 }
 
-func Compress(b64 string, resolution int, name string, compressed *bytes.Buffer) {
+func Compress(b64 string, resolution int, name string, compressed *bytes.Buffer) error {
 	// Log("Base64 is of length " + fmt.Sprint(len(b64)) + " for " + name)
 	data, err := base64.StdEncoding.DecodeString(b64)
-	Assert(err, "Failed to decode base64 of image")
+	if err != nil {
+		return fmt.Errorf("Failed to decode base64 of image: %w", err)
+	}
 	// Log("Decoded base64 is of length " + fmt.Sprint(len(data)))
 
 	srcimg, err := imaging.Decode(strings.NewReader(string(data)))
-	Assert(err, "Failed to decode image from data")
+	if err != nil {
+		return fmt.Errorf("Failed to decode image from data: %w", err)
+	}
 
 	// Lanczos my beloved 💖 (change it to something faster idc)
 	img := imaging.Resize(srcimg, resolution, resolution, imaging.Lanczos)
-	Assert(err, "Failed to create image from data")
+	if img == nil {
+		return errors.New("Failed to create image from data")
+	}
 
-	imaging.Encode(compressed, img, imaging.PNG)
 	// Log("Compressed image is of length " + fmt.Sprint(compressed.Len()))
-
-	wg.Done()
+	return imaging.Encode(compressed, img, imaging.PNG)
 }
 
 func idRoute(w http.ResponseWriter, r *http.Request) {
@@ -121,7 +125,11 @@ func idRoute(w http.ResponseWriter, r *http.Request) {
 	}
 
 	loadScript, err := io.ReadAll(r.Body)
-	Assert(err, "Failed to read render script")
+	if err != nil {
+		Log(c.InRed("Failed to read render script: " + err.Error()))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
 	script := strings.ReplaceAll(string(loadScript), "_PING_URL", "http://localhost:64990/ping")
 
@@ -130,18 +138,30 @@ func idRoute(w http.ResponseWriter, r *http.Request) {
 	currentTemplate = strings.ReplaceAll(currentTemplate, "_RENDER_SCRIPT", script)
 
 	req, err := http.NewRequest("POST", "http://localhost:64989", strings.NewReader(currentTemplate))
-	Assert(err, "Failed to create request")
+	if err != nil {
+		Log(c.InRed("Failed to create request: " + err.Error()))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
 	req.Header.Set("Content-Type", "text/xml; charset=utf-8")
 	req.Header.Set("SOAPAction", "http://roblox.com/OpenJobEx")
 
 	Log(c.InBlue("Sending request to render " + id))
 	res, err := client.Do(req)
-	Assert(err, "Failed to send request to RCCService")
+	if err != nil {
+		Log(c.InRed("Failed to send request to RCCService: " + err.Error()))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	defer res.Body.Close()
 
 	body, err := io.ReadAll(res.Body)
-	Assert(err, "Failed to read response from RCCService")
+	if err != nil {
+		Log(c.InRed("Failed to read response from RCCService: " + err.Error()))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	fmt.Println(string(body))
 
 	Log(c.InGreen("Render " + id + " started"))
@@ -164,14 +184,26 @@ func pingIdRoute(w http.ResponseWriter, r *http.Request) {
 	id := r.PathValue("id")
 
 	readBody, err := io.ReadAll(r.Body)
-	Assert(err, "Failed to read request body")
+	if err != nil {
+		Log(c.InRed("Failed to read request body: " + err.Error()))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
 	// if the body is gzipped, unzip it
 	if strings.HasPrefix(string(readBody), "\x1f\x8b") {
 		reader, err := gzip.NewReader(strings.NewReader(string(readBody)))
-		Assert(err, "Failed to create gzip reader")
+		if err != nil {
+			Log(c.InRed("Failed to create gzip reader: " + err.Error()))
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 		readBody, err = io.ReadAll(reader)
-		Assert(err, "Failed to read gzipped request body")
+		if err != nil {
+			Log(c.InRed("Failed to read gzipped request body: " + err.Error()))
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 	}
 
 	data := strings.Split(string(readBody), "\n")
@@ -185,16 +217,31 @@ func pingIdRoute(w http.ResponseWriter, r *http.Request) {
 	case "Rendering":
 		Log(c.InGreen("Render " + id + " is rendering"))
 	case "Completed":
-		num := len(data) - 1
-		wg.Add(num)
+		var wg sync.WaitGroup
 
 		// Could result in a random order if appending to an array instead
 		var body, head bytes.Buffer
-		go Compress(data[1], 420, "body", &body)
-		if num == 2 {
-			go Compress(data[2], 150, "head", &head)
+		var bodyErr, headErr error
+		wg.Go(func() {
+			bodyErr = Compress(data[1], 420, "body", &body)
+		})
+		if len(data) == 3 {
+			wg.Go(func() {
+				headErr = Compress(data[2], 150, "head", &head)
+			})
 		}
 		wg.Wait()
+
+		if bodyErr != nil {
+			Log(c.InRed("Failed to compress body image: " + bodyErr.Error()))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if headErr != nil {
+			Log(c.InRed("Failed to compress head image: " + headErr.Error()))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 
 		binary.Write(&encoded, binary.BigEndian, uint32(body.Len()))
 		binary.Write(&encoded, binary.BigEndian, uint32(head.Len()))
@@ -207,21 +254,24 @@ func pingIdRoute(w http.ResponseWriter, r *http.Request) {
 	// Send to server as binary
 	endpoint := fmt.Sprintf("%s/%s?apiKey=%s", os.Getenv("ENDPOINT"), id, apiKey)
 	// We (still) have to lie about the contentType to avoid being nuked by CORS from the website
-	_, err = http.Post(endpoint, "text/json", &encoded)
-	Assert(err, "Failed to send render data to server")
+	if _, err = http.Post(endpoint, "text/json", &encoded); err != nil {
+		Log(c.InRed("Failed to send render data to server: " + err.Error()))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
 	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
 	Log(c.InYellow("Loading environment variables..."))
-	err := env.Load(".env")
-	Assert(err, "Failed to load environment variables. Please place them in a .env file in the current directory.")
+	Fatal(env.Load(".env"), "Failed to load environment variables. Please place them in a .env file in the current directory.")
 
 	Log(c.InPurple("Starting RCCService..."))
 	go StartRCC()
 
 	loaded := false
+	// goroutines mean it'll actually do it every 100ms
 	for startTime := time.Now(); !loaded; time.Sleep(100 * time.Millisecond) {
 		go TestRCCStarted(&loaded, int(time.Since(startTime).Seconds()))
 	}
@@ -229,11 +279,10 @@ func main() {
 	fmt.Println()
 	Log(c.InPurple("Starting server..."))
 
-	router := http.NewServeMux()
-	router.HandleFunc("POST /{id}", idRoute)
-	router.HandleFunc("POST /ping/{id}", pingIdRoute)
+	http.HandleFunc("POST /{id}", idRoute)
+	http.HandleFunc("POST /ping/{id}", pingIdRoute)
 
-	Log(c.InGreen("~ RCCService is up on port 64990 ~"))
+	Log(c.InGreen("~ RCCService proxy is up on port 64990 ~"))
 	Log(c.InGreen("Send a POST request to /{your task id} with the render script as the body to start a render"))
-	http.ListenAndServe(":64990", router)
+	http.ListenAndServe(":64990", nil)
 }
