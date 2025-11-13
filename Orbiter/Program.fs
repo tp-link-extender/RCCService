822a3023422e34ca508d363640222b273e5ad386
Lewin Kelly <heli@odex.cf>
2025-11-13 03:18:01 +0000

    Attempt to start process and (unsuccessfully) to get window title

--- Orbiter/Program.fs
+++ Orbiter/Program.fs
@@ -1,6 +1,8 @@
 ﻿open System.IO
 open Microsoft.FSharp.Core.Result
 open System
+open System.Diagnostics
+open System
 
 let (>>=) f x = bind x f
 
@@ -17,34 +19,93 @@ let getStudio () =
 
 let getArgs (id: int) (path: string) =
     // get current operating system
-    let start =
-        if OperatingSystem.IsLinux() then
-            [ "wine" ]
-        else
-            []
-
     let args =
-        start
-        @ [ path
-            "--script"
-            $"dofile(\"http://mercs.dev/game/{id}/serve\")" ]
+        [ path
+          "--script"
+          $"dofile(\"http://mercs.dev/game/{id}/serve\")" ]
 
-    Ok args
+    Ok(
+        if OperatingSystem.IsLinux() then
+            [ "wine" ] @ args
+        else
+            args
+    )
+
+let startGameserver (args: string list) =
+    try
+        let startInfo = ProcessStartInfo()
+        startInfo.FileName <- args.Head
+        startInfo.Arguments <- String.Join(" ", args.Tail)
+        startInfo.UseShellExecute <- false
+        startInfo.RedirectStandardOutput <- true
+        startInfo.RedirectStandardError <- true
+
+        let proc = Process.Start startInfo
+        proc.EnableRaisingEvents <- true
+
+        proc.OutputDataReceived.Add(fun data -> printfn $"[STDOUT] {data.Data}")
+        // proc.ErrorDataReceived.Add(fun data -> printfn $"[STDERR] {data.Data}")
+
+        proc.BeginOutputReadLine()
+        proc.BeginErrorReadLine()
+
+        Ok proc
+    with
+    | ex -> Error $"Failed to start gameserver: {ex.Message}"
+
+let pollMonitor (getter: unit -> 'a) (callback: 'a -> unit) =
+    let rec loop () =
+        async {
+            callback (getter ())
+            do! Async.Sleep 1000
+            return! loop ()
+        }
+
+    Async.Start(loop ())
+
+let monitorGameserver (proc: Process) =
+    proc.Exited.Add(fun _ -> printfn "[INFO] Gameserver process has exited.")
+
+    // get when the process is responding
+    pollMonitor
+        (fun () -> proc.Responding)
+        (fun responding ->
+            if responding then
+                printfn "[INFO] Gameserver process is responding."
+            else
+                printfn "[WARN] Gameserver process is not responding."
+
+            let success = proc.CloseMainWindow()
+
+            if not success then
+                printfn "[WARN] Failed to close main window of gameserver process.")
+
+
+    |> ignore
+
+    // print whenever window title changes
+    proc.WaitForExitAsync()
+    |> Async.AwaitTask
+    |> Async.RunSynchronously
+
+    Ok()
+
 
 let newGameserver (id: int) =
     getStudio ()
     >>= log
     >>= getArgs id
     >>= log
-    >>= fun path ->
-            printfn $"Starting gameserver with ID {id} using studio at {path}"
-            Ok()
-
+    >>= startGameserver
+    >>= log
+    >>= monitorGameserver
 
 let start (id: int) =
     printfn $"Received start request for ID {id}"
 
     newGameserver id
+
+// wait for all async tasks to complete
 
 [<EntryPoint>]
 let main _ =
